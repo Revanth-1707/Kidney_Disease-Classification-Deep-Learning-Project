fd8ee0d38a150589f98f1c398517a65e775ed99e
Revanth-1707 <revanthagastya373@gmail.com>
2026-06-22 13:14:21 +0530

    prepare base model added

---
+++ src/cnnClassifier/pipeline/stage_02_prepare_base_model.py
@@ -0,0 +1,29 @@
+from cnnClassifier.components.prepare_base_model import PrepareBaseModel
+from cnnClassifier.config.configuration import ConfigurationManager
+from cnnClassifier import logger
+
+STAGE_NAME = "PREPARE_BASE_MODEL"
+
+class PrepareBaseModelTrainingPipeline:
+
+    def __init__(self):
+        pass
+
+    def main(self):
+        config = ConfigurationManager()
+        prepare_base_model_config = config.get_prepare_base_model_config()
+
+        prepare_base_model = PrepareBaseModel(config=prepare_base_model_config)
+        prepare_base_model.get_base_model()
+        prepare_base_model.update_base_model()
+
+if __name__ == "__main__":
+    try:
+        logger.info(f">>>>> stage {STAGE_NAME} started <<<<<")
+        prepare_base_model_training_pipeline = PrepareBaseModelTrainingPipeline()
+        prepare_base_model_training_pipeline.main()
+        logger.info(f">>>>> stage {STAGE_NAME} completed <<<<<\n\nx==========x")
+    except Exception as e:
+        logger.exception(e)
+        raise e
+
